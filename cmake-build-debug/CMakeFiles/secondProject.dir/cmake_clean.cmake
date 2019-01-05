01514211fe79be7881daa989138426b112c45b78
eliran0220 <eliran0220@gmail.com>
2019-01-06 00:15:34 +0200

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/secondProject.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/secondProject.dir/main.cpp.o"
+  "secondProject.pdb"
+  "secondProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/secondProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/secondProject.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for secondProject.
+# This may be replaced when dependencies are built.
